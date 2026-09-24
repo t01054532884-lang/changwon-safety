6cc72c14f787b3c93eb2720be4946f4c4d16a64f
t01054532884-lang <t01054532884@gmail.com>
2026-09-24 19:11:30 +0900

    Update sw.js

--- citizen-app/frontend/sw.js
+++ citizen-app/frontend/sw.js
@@ -15,14 +15,14 @@ const APP_SHELL = [
   "./vendor/leaflet/images/marker-icon-2x.png",
   "./vendor/leaflet/images/marker-shadow.png",
 ];
-
+ 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => {})
   );
   self.skipWaiting();
 });
-
+ 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
@@ -31,7 +31,7 @@ self.addEventListener("activate", (event) => {
   );
   self.clients.claim();
 });
-
+ 
 // 2026-09-24 변경: 예전엔 "캐시 우선"이라서 배포 후에도 브라우저가 예전 화면을
 // 계속 보여주는 문제가 있었다(sw.js 자체가 바뀌지 않으면 새 버전 감지도 안 됨).
 // 이제는 "네트워크 우선"으로 바꿔서, 온라인이면 항상 최신 파일을 받아오고
@@ -48,3 +48,4 @@ self.addEventListener("fetch", (event) => {
       .catch(() => caches.match(event.request))
   );
 });
+ 
